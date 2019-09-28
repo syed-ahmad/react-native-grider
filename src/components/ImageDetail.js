51c000ea7ed626450ef3ec8d4a8cf45921fa618e
Syed Ahmad <syed-ahmad@outlook.com>
2019-09-29 00:12:35 +0100

    Show score prop on ImageDetail

--- src/components/ImageDetail.js
+++ src/components/ImageDetail.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { View, Text, StyleSheet, Image } from 'react-native';
 
-const ImageDetail = ({ title, imageSource }) => {
+const ImageDetail = ({ title, imageSource, score }) => {
   return (
     <View>
       <Text>{title}</Text>
+      <Text>{`Image score - ${score}`}</Text>
       <Image source={imageSource} />
     </View>
   );
