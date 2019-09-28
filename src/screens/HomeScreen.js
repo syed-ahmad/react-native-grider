ec1d5ac3b774340da53029aff57dc61b963b1d93
Syed Ahmad <syed-ahmad@outlook.com>
2019-09-28 23:33:51 +0100

    Use Buttons for now

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,26 +1,20 @@
 import React from 'react';
 import { Text, StyleSheet, View, Button, TouchableOpacity } from 'react-native';
 
-const HomeScreen = () => {
+const HomeScreen = props => {
   return (
     <View>
       <Text style={styles.text}>Hola kala</Text>
       <Button
         title="Go to Components Demo"
         onPress={() => {
-          console.log('button pressed!');
+          props.navigation.navigate('Components');
         }}
       />
-      <TouchableOpacity
-        onPress={() => {
-          console.log('List Pressed!');
-        }}
-      >
-        <Text>Go to List Demo</Text>
-        <Text>Go to List Demo</Text>
-        <Text>Go to List Demo</Text>
-        <Text>Go to List Demo</Text>
-      </TouchableOpacity>
+      <Button
+        title="Got to List Demo"
+        onPress={() => props.navigation.navigate('List')}
+      />
     </View>
   );
 };
