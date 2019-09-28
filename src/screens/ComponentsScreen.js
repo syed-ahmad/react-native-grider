d4af3b0175b5ba1000b6abd1603336d4a847d000
Syed Ahmad <syed-ahmad@outlook.com>
2019-09-28 22:09:50 +0100

    Apply styles to two text components

--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -2,18 +2,21 @@ import React from 'react';
 import { Text, StyleSheet, View } from 'react-native';
 
 const ComponentsScreen = () => {
-  const greeting = <Text>Hey man</Text>;
+  const name = 'Syed';
   return (
     <View>
-      <Text style={styles.textStyle}>This is the ComponentScreen</Text>
-      {greeting}
+      <Text style={styles.textStyle}>Getting started with React Native!</Text>
+      <Text style={styles.subHeaderStyle}>My name is {name}</Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   textStyle: {
-    fontSize: 30
+    fontSize: 40
+  },
+  subHeaderStyle: {
+    fontSize: 20
   }
 });
 
