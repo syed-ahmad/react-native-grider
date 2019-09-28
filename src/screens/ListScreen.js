4e8b1e9870ef04c1da84a1dde403eba854018ad5
Syed Ahmad <syed-ahmad@outlook.com>
2019-09-28 22:42:48 +0100

    Remove horizontal scroll bar on FlatList

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -17,15 +17,21 @@ const ListScreen = () => {
 
   return (
     <FlatList
+      horizontal
+      showsHorizontalScrollIndicator={false}
       keyExtractor={friend => friend.name}
       data={friends}
       renderItem={({ item }) => {
-        return <Text>{item.name}</Text>;
+        return <Text style={styles.textStyle}>{item.name}</Text>;
       }}
     ></FlatList>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  textStyle: {
+    marginVertical: 50
+  }
+});
 
 export default ListScreen;
