b26e09fed769ebd9219ce0ffdbf57b9c3b8599b1
Syed Ahmad <syed-ahmad@outlook.com>
2019-09-29 00:29:52 +0100

    Use state

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,22 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
 
 const CounterScreen = () => {
-  let counter = 0;
+  const [counter, setCounter] = useState(0);
   return (
     <View>
       <Button
         title="Increase"
         onPress={() => {
-          counter++;
-          console.log(counter);
+          setCounter(counter + 1);
         }}
       />
       <Button
         title="Decrease"
         onPress={() => {
-          counter--;
-          console.log(counter);
+          setCounter(counter - 1);
         }}
       />
       <Text>{`Counter Count : ${counter}`}</Text>
